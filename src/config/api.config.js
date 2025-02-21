11bf407fbbc54bc8aaef62287f9531f6ef1b52c1
Muhammad Shoaib <sendmailtoshoaib@gmail.com>
2025-02-21 17:08:59 +0500

    changes 21 feb 2025

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-    BASE_URL: 'http://195.179.231.102:6003',
-    API_URL: 'http://195.179.231.102:6003/api'
+    BASE_URL: '',  // Empty string for same-origin requests
+    API_URL: '/api'  // Just /api since it's now on the same origin
 };
 
 export default API_CONFIG;
